a48cd2563c0b9a6733c45a2d6ed11650f6184c9a
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-09-19 17:42:02 -0300

    starting

---
+++ src/test/java/com/services/group4/parser/ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.group4.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
