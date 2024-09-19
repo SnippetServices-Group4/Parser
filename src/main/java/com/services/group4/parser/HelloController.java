61b8ead5488aad32719037d6a6b213ca147e8658
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-09-19 18:11:24 -0300

    hello world

---
+++ src/main/java/com/services/group4/parser/HelloController.java
@@ -0,0 +1,22 @@
+package com.services.group4.parser;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/echo")
+    public String echo(@RequestBody String message) {
+        return message;
+    }
+}
