ae601fc5a06826d3f7b31f63c93dbfe339826e97
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-22 14:41:20 -0300

    Deleted unused imports.

--- src/test/java/com/services/group4/parser/ModulesCommunicationTest.java
+++ src/test/java/com/services/group4/parser/ModulesCommunicationTest.java
@@ -1,18 +1,11 @@
 package com.services.group4.parser;
 
-import com.services.group4.parser.model.CommunicationMessage;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
