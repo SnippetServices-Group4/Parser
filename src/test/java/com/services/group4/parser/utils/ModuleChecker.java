237121342f00aef63c4cc07f12b268ba159c12f1
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-22 14:37:33 -0300

    Created a util class that checks if a determined module is on.

---
+++ src/test/java/com/services/group4/parser/utils/ModuleChecker.java
@@ -0,0 +1,15 @@
+package com.services.group4.parser.utils;
+
+import org.springframework.web.client.RestTemplate;
+
+public class ModuleChecker {
+  public static boolean isSnippetModuleRunning() {
+    try {
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.getForObject("http://localhost:8080/test/parser/communication", String.class);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
