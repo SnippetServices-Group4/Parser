b846b7338ad4b6905cea41c219e40ea4715a44db
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-09-26 18:46:36 -0300

    Dockerization (database removal and spring now running in a container)

--- src/main/java/com/services/group4/parser/ParserApplication.java
+++ src/main/java/com/services/group4/parser/ParserApplication.java
@@ -2,9 +2,9 @@ package com.services.group4.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class ParserApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class ParserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParserApplication.class, args);
