86ba3c95d8a28bb59bbf99c1968624db5f272448
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-11 12:12:19 -0300

    Spotless apply (sape)

--- src/main/java/com/services/group4/parser/ParserApplication.java
+++ src/main/java/com/services/group4/parser/ParserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class ParserApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParserApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ParserApplication.class, args);
+  }
 }
