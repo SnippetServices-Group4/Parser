5a8836dbb8ce66c5b60110af860439f98ef3146e
Agustin <almartinez1@mail.austral.edu.ar>
2024-11-20 13:48:03 -0300

    add logs

--- src/main/java/com/services/group4/parser/services/ParserService.java
+++ src/main/java/com/services/group4/parser/services/ParserService.java
@@ -19,14 +19,7 @@ import com.services.group4.parser.services.adapter.LintConfigAdapter;
 import com.services.group4.parser.services.utils.OutputListString;
 import input.InputHandler;
 import input.InputQueue;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.Queue;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,6 +30,16 @@ import output.OutputResult;
 import output.OutputString;
 import runner.Runner;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Queue;
+
+@Slf4j
 @Service
 public class ParserService {
   List<Language> languages;
@@ -54,15 +57,18 @@ public class ParserService {
   }
 
   private void validateLanguage(String language, String version) {
+    log.info("Validating snippet language and version: {}", language);
     if (languages.stream()
         .noneMatch(l -> l.getLangName().equals(language) && l.getVersion().contains(version))) {
+      log.info("Language or version not supported: {} , {}", language, version);
       throw new NoSuchElementException("Language not supported");
     }
+    log.info("Language and version validated: {} {}", language, version);
   }
 
   // TODO: execute should switch on language
   public ResponseEntity<ResponseDto<ExecuteResultDto>> execute(
-      Long snippetId, ProcessingRequestDto request) {
+    Long snippetId, ProcessingRequestDto request) {
     String language = request.language().toLowerCase();
     String version = request.version();
 
@@ -90,13 +96,19 @@ public class ParserService {
     InputHandler inputHandler = new InputQueue(inputQueue);
 
     try {
+      log.info("Executing snippet with id: {}", snippetId);
       runner.execute(stream, version, printLog, errorLog, inputHandler);
+      log.info("Snippet executed successfully: {}", snippetId);
+      log.debug("Print log: {}, Error log: {}", printLog.getResult(), errorLog.getResult());
+
       return FullResponse.create(
           "Snippet executed successfully",
           "executeResult",
           new ExecuteResultDto(snippetId, printLog.getResult(), errorLog.getResult()),
           HttpStatus.OK);
     } catch (Exception e) {
+      log.error("Snippet execution failed: {}", snippetId);
+      log.debug("Error log: {}", e.getMessage());
       return FullResponse.create(
           "Snippet execution failed", "executeResult", null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -118,11 +130,13 @@ public class ParserService {
     Queue<String> inputQueue = new LinkedList<>(request.inputs());
     InputHandler inputHandler = new InputQueue(inputQueue);
 
+    log.info("Trying to run tests for snippet with id: {}", request.snippetId());
     try {
       runner.execute(stream, request.version(), testOutput, errorOutput, inputHandler);
 
       boolean success = testOutput.getListString().equals(request.outputs());
 
+      log.info("Tests ran successfully: {}", request.snippetId());
       return FullResponse.create(
           "Test ran successfully",
           "executedTest",
@@ -130,6 +144,7 @@ public class ParserService {
               request.snippetId(), request.testId(), success ? TestState.PASSED : TestState.FAILED),
           HttpStatus.OK);
     } catch (Exception e) {
+      log.error("Tests failed for snippet with id: {}", request.snippetId());
       return FullResponse.create(
           "Something went wrong when executing the tests",
           "executedTest",
@@ -155,14 +170,22 @@ public class ParserService {
     String rules = formatConfigAdapter.adaptFormatConfig(request.getFormatRules());
 
     try {
+      log.info("Trying to format snippet with id: {}", snippetId);
       String output = format(snippet.get(), version, rules);
+      log.info("Snippet formatted successfully: {}", snippetId);
+      log.debug("Formatted snippet with id {} using rules: {}, version: {}", snippetId, rules, version);
+
+      log.info("Trying to save formatted snippet in bucket");
       bucketClient.saveSnippet(container, snippetId, output);
+      log.info("Formatted snippet successfully saved in bucket");
       return FullResponse.create(
           "Snippet formatting executed successfully",
           "formatResult",
           new FormattingResultDto(snippetId, output, language, version, rules),
           HttpStatus.OK);
     } catch (Exception e) {
+      log.error("Snippet formatting with id: {}", snippetId + "failed");
+      log.debug("Error log: {}", e.getMessage());
       return FullResponse.create(
           "Snippet formatting failed", "formatResult", null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -194,7 +217,9 @@ public class ParserService {
     String rules = lintConfigAdapter.adaptLintConfig(request.getLintRules());
 
     try {
+      log.info("Trying to lint snippet with id: {}", snippetId);
       OutputReport report = lint(snippet.get(), version, rules);
+      log.info("Snippet linted successfully: {}", snippetId);
       return FullResponse.create(
           "Linting executed successfully",
           "lintResult",
@@ -202,6 +227,8 @@ public class ParserService {
               snippetId, report.getFullReport().getReports(), language, version, rules),
           HttpStatus.OK);
     } catch (Exception e) {
+      log.error("Snippet linting with id: {}", snippetId + "failed");
+      log.debug("Error log: {}", e.getMessage());
       return FullResponse.create(
           "Linting failed",
           "lintResult",
@@ -233,6 +260,7 @@ public class ParserService {
     ValidationState state;
     String report;
 
+    log.info("Trying to validate snippet with id");
     try {
       runner.validate(stream, version);
       state = ValidationState.VALID;
@@ -240,8 +268,10 @@ public class ParserService {
     } catch (Exception e) {
       state = ValidationState.INVALID;
       report = e.getMessage();
+      log.debug("Snippet validation failed: {}", e.getMessage());
     }
 
+    log.info("Snippet validated: {}", state);
     return FullResponse.create(
         "Validation executed: " + report, "validationResult", state, HttpStatus.OK);
   }
